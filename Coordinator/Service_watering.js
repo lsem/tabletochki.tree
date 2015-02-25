62300d0ac77d8eb86eda50f1432c8d4193c18ade
Lyubomyr Semkiv <lyubomyr.semkiv@hotmail.com>
2015-02-25 18:24:06 +0200

    Polished watering js service, fixed connection restoring logic

--- Coordinator/Service_watering.js
+++ Coordinator/Service_watering.js
@@ -1,133 +1,56 @@
-var types = require('./types');
-var master = require('./svcutils').master;
-var coordinator = require('./svcutils').coordinator(types.Services.Watering);
-var logger = require('./log_manager.js').loggerFor(types.Services.Watering);
-var hardwareClient = require('./hardwareClient');
-
 var thrift = require('thrift');
 var ThriftTransports = require('thrift/lib/thrift/transport');
 var ThriftProtocols = require('thrift/lib/thrift/protocol');
 var HardwareService = require('./gen-nodejs/HardwareService');
 var HardwareServiceTypes = require('./gen-nodejs/HardwareService_types');
-
-
-var hardwareClientProxy = null;
-
+var types = require('./types');
+var master = require('./svcutils').master;
+var coordinator = require('./svcutils').coordinator(types.Services.Watering);
+var logger = require('./log_manager.js').loggerFor(types.Services.Watering);
 
 var transport = ThriftTransports.TFramedTransport();
 var protocol = ThriftProtocols.TBinaryProtocol();
-var serviceApiClient =  null;
 
-var stateJson = {}
+var jsUnitServiceState = {
+    health: 'green',
+    details: {}
+};
 
-var thriftConnection = thrift.createConnection("localhost", 9090, {transport: transport, protocol: protocol});
-
-thriftConnection.on('connect', function () {
-    serviceApiClient = thrift.createClient(HardwareService, thriftConnection);
-});
-thriftConnection.on('error', function () {
-    logger.error('Service API connection lost');
-    serviceApiClient = null;
-    currentStatus.hardStatus = -1;
-});
-
-logger.info('starting service poller...');
+var thriftConnection = null;
 
 setInterval(function () {
-    //logger.info('**************************');
-    //logger.info('**************************');
-    //logger.info('**************************');
+    jsUnitServiceState.details = {};
 
-
+    if (thriftConnection === null || (typeof thriftConnection == 'undefined')) {
+        logger.error('establishing thrift connection');
+        thriftConnection = thrift.createConnection("localhost", 9090, {transport: transport, protocol: protocol});
+        thriftConnection.on('error', function() {
+            logger.error('thrift tcp connection error');
+            thriftConnection = null;
+        });
+    }
+    var serviceApiClient = thrift.createClient(HardwareService, thriftConnection);
     if (serviceApiClient !== null) {
         serviceApiClient.getServiceStateJson(function (err, data) {
-                if (err) {
-                    logger.error('Error getting service detailed state (JSON)');
-                }
-                else {
-                    if (data) {
-                        //logger.debug('state json received: '+ data);
-                        stateJson = JSON.parse(data);
-                        serviceState.details = stateJson;
-                        //currentStatus.hardStatus = data.statusCode;
-                    }
-                    else {
-                        logger.error('Detailed service state returned data in unsupported format');
-                    }
-                }
-            });
-
-    }
-    else {
+            if (err) {
+                logger.error('Error getting service detailed state (JSON)');
+            } else if (data) {
+                jsUnitServiceState.details = JSON.parse(data);
+            } else {
+                logger.error('Detailed service state returned data in unsupported format');
+            }
+        });
+    } else {
         logger.error('Cannot update hardware service detailed status: no API connection');
     }
-
 }, 100);
 
-hardwareClient.connect({
-    onConnected: function(client) {
-        //logger.info('Hardware service connected');
-        master.raiseMessage(types.WateringEvents.HardwareConnected, 0, {});
-        hardwareClientProxy = client;
-    },
-    onFailed: function() {
-        //logger.error('Hardware service connection error');
-        master.raiseMessage(types.WateringEvents.HardwareDisconnected, 0, {});
-
-        hardwareClientProxy = null;
-    }
-});
-
-var serviceState = {
-    health: 'green'
-
-};
 process.on('message', function(m) {
     if (m.eventId === types.serviceEvents.GetStatus) {
-        coordinator.sendMessage(types.serviceEvents.StatusResponse, serviceState);
+        coordinator.sendMessage(types.serviceEvents.StatusResponse, jsUnitServiceState);
+    } else {
+        logger.warning('Unhandled message: ' + JSON.stringify(m));
     }
 });
 
-
 master.raiseMessage(types.Messages.Service.ServiceStarted);
-
-
-// This service basically can handle next messages:
-//      - pumpWater(destinationTankId, sourceTankId, out pumpID);
-//
-// Besides these, it also have reverse messages (obtained by polling):
-//      - button pressed
-//      - status changed(Status status, Id id);
-//
-
-// Status changed means that
-
-
-
-
-
-//// Message handlers
-//process.on('message', function(m) {
-//    logger.warning('Message received: '  + JSON.stringify(m));
-//
-//    //if (m.messageType === types.Messages.Service.Close) {
-//    //    logger.info('Close requested');
-//    //
-//    //}
-//    //// todo: add other service message handlers
-//    //else if (m.messageType === type.Messages.Coordinator.GetHardwareInput) {
-//    //    logger.info('Get input requested');
-//    //}
-//    //else if (m.messageType === type.Messages.Cooridnator.StartPump) {
-//    //    logger.info('Start pump requested');
-//    //// TODO: Get pump Id
-//    ////hardwareClient.startPump(...);
-//    //// ...
-//    //}
-//    //else if (m.messageType === type.Messages.Coordinator.StopPump) {
-//    //    logger.info('Stop pump requested');
-//    ////hardwareClient.stopPump(...);
-//    //// TODO: Get pump Id
-//    //}
-//
-//});
