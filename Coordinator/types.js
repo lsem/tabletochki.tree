7648bfb4a8f63e7309a643837e9bc7fce4bf2e4d
Lyubomyr Semkiv <lyubomyr.semkiv@hotmail.com>
2015-02-26 18:10:16 +0200

    Removed unused types in Coordinator

--- Coordinator/types.js
+++ Coordinator/types.js
@@ -1,17 +1,5 @@
 
 
-exports.serviceEvents = {
-    ServiceStarted: 0,
-    Close: 1,
-    Initialize: 2,
-    Finalize: 3,
-    UserEvent: 4,
-    GetStatus: 5,
-    StatusResponse: 6,
-    AggregatedServicesStatus: 7,
-    AggregatesServiceStatusResponse: 8
-};
-
 exports.Services = {
     Watering: 'Svc_Watering',
     Kinect: 'Svc_Kinect',
@@ -19,45 +7,4 @@ exports.Services = {
     AdminUI: 'Svc_AdminUI',
     Coordinator: 'Coordinator'
 };
-
-exports.WateringEvents = {
-    HardwareConnected: "HardwareConnected",
-    HardwareDisconnected: "HardwareDisconnected"
-};
-
-exports.Messages = {
-        Service: {
-            ServiceStarted: 'ServiceStarted',
-            Close: 'Close',
-            Initialize: 'Initialize',
-            Finalize: 'Finalize',
-            UserEvent: 'UserEvent'
-        },
-
-        HttpListener: {
-            DonationCommited: 'DonationCommited'
-        },
-
-        Watering: {
-            HardwareConnected: 'HardwareConnected',
-            HardwareDisconnected: 'HardwareDisconnected',
-            HardwareInput: 'HardwareInput'
-        },
-
-        Coordinator: {
-            GetHardwareInput: 'GetHardwareInput',
-            Pour: 'Pour'
-        }
-};
-
-
-exports.watering =  {
-    PumpWater: 0
-};
-
-exports.types = {
-  Pump: 0
-};
-
-
 
